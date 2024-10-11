129b9a2eba56208efd1a7c9138d99fe7c10612fa
elijah <layjmadonna@gmail.com>
2024-10-11 14:17:37 +0800

    employee_list.py

---
+++ employee_list.py
@@ -0,0 +1,19 @@
+employee_name_list = ["John Doe", "Jane Smith", "Mark Johnson", "Lisa Wong", "Paul McDonald"]
+
+print(employee_name_list)
+
+employee_ID_list = [1, 2, 3, 4, 5]
+
+print(employee_ID_list)
+
+employee_department_list = ["Sales", "Human Resources", "IT", "Marketing", "Finance"]
+
+print(employee_department_list)
+
+employee_age_list= [30, 25, 40, 28, 35]
+
+print(employee_age_list)
+
+employee_email_list = ["john.doe@company.com", "jane.smith@company.com", "mark.johnson@company.com", "lisa.wong@company.com", "paul.mcdonald@company.com"]
+
+print(employee_email_list)
