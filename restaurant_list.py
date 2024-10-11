de752f708ed410b6999474836b552558058030eb
elijah <layjmadonna@gmail.com>
2024-10-11 14:25:59 +0800

    restaurant_list.py

---
+++ restaurant_list.py
@@ -0,0 +1,20 @@
+restaurant_name_list = ["Vikings Luxury Buffet", "Antonio's Restaurant", "Mesa Filipino Moderne", "Manam Comfort Filipino", "Ramen Nagi"]
+print(restaurant_name_list)
+
+
+restaurant_ID_list = [1, 2, 3, 4, 5]
+print(restaurant_ID_list)
+
+
+restaurant_location_list = ["Pasay City", "Tagaytay", "Makati City", "Quezon City", "Various Locations"]
+print(restaurant_location_list)
+
+
+restaurant_cuisine_type_list = ["Buffet", "Fine Dining", "Filipino", "Filipino", "Japanese"]
+print(restaurant_cuisine_type_list)
+
+restaurant_established_year_list = [2011, 2002, 2009, 2013, 2013]
+print(restaurant_established_year_list)
+
+restaurant_website_list = ["www.vikings.ph", "www.antoniosrestaurant.ph", "www.mesa.ph", "www.manam.ph", "www.ramennagi.com.ph"]
+print(restaurant_website_list)
