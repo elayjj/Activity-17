f7b09b0973a8fa5375c38c02679c5b6072bbc2bb
elijah <layjmadonna@gmail.com>
2024-10-11 14:10:42 +0800

    product_list.py

---
+++ product_list.py
@@ -0,0 +1,23 @@
+product_name_list = ["Laptop", "Desk Chair", "Smartwatch", "Notebook", "Running Shoes"]
+
+print(product_name_list)
+
+product_ID_list = [1, 2, 3, 4, 5]
+
+print(product_ID_list)
+
+product_category_list = ["Electronics", "Furniture", "Electronics", "Stationery", "Apparel"]
+
+print(product_category_list)
+
+product_price_list = [750, 100, 200, 5, 80]
+
+print(product_price_list)
+
+product_stock_list = [50, 200, 150, 500, 100]
+
+print(product_stock_list)
+
+product_supplier_email_list = ["supplier1@gmail.com", "supplier2@gmail.com", "supplier3@gmail.com", "supplier4@gmail.com", "supplier5@gmail.com"]
+
+print(product_supplier_email_list)
