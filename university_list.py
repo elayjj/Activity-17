1c1946bb257532a513c110a8f4167b7150e4988e
elijah <layjmadonna@gmail.com>
2024-10-11 14:23:41 +0800

    university_list.py

---
+++ university_list.py
@@ -0,0 +1,17 @@
+university_name_list = ["University of the Philippines", "Ateneo de Manila University", "De La Salle University", "University of Santo Tomas", "Polytechnic University of the Philippines"]
+print(university_name_list)
+
+university_ID_list = [1, 2, 3, 4, 5]
+print(university_ID_list)
+
+university_location_list = ["Quezon City", "Quezon City", "Manila", "Manila", "Manila"]
+print(university_location_list)
+
+university_established_year_list = [1908, 1859, 1911, 1611, 1904]
+print(university_established_year_list)
+
+university_type_list = ["Public", "Private", "Private", "Private", "Public"]
+print(university_type_list)
+
+university_website_list = ["www.up.edu.ph", "www.ateneo.edu", "www.dlsu.edu.ph", "www.ust.edu.ph", "www.pup.edu.ph"]
+print(university_website_list)
