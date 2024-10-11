ee54ea7d6ce12f244bd36acb18de86a85901fc5a
elijah <layjmadonna@gmail.com>
2024-10-11 14:20:46 +0800

    book_list.py

---
+++ book_list.py
@@ -0,0 +1,31 @@
+book_title_list = ["The Great Gatsby", "To Kill a Mockingbird", 1984, "The Catcher in the Rye", "A Brief History of Time"]
+
+print(book_title_list)
+
+book_author_list = ["F. Scott Fitzgerald", "Harper Lee", "George Orwell", "J.D. Salinger", "Stephen Hawking"]
+
+print(book_author_list)
+
+book_ID_list = [1, 2, 3, 4, 5]
+
+print(book_ID_list)
+
+book_genre_list = ["Fiction", "Fiction", "Dystopian", "Fiction", "Non-Fiction"]
+
+print(book_genre_list)
+
+book_published_year_list = [1925, 1960, 1949, 1951, 1988]
+
+print(book_published_year_list)
+
+book_ISBN_list = ["978-0743273565", "978-0060935467", "978-0451524935", "978-0316769488", "978-0553380163"]
+
+print(book_ISBN_list)
+
+book_stock_list = [20, 35, 40, 25, 10]
+
+print(book_stock_list)
+
+book_price_list = [15.99, 10.99, 9.99, 8.99, 18.99]
+
+print(book_price_list)
